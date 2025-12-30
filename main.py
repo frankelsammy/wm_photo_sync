b1ccd74c50fc025ad3529ef484c5762b0d656ea9
Sammy Frankel <frankelsammy31@gmail.com>
2025-12-30 12:50:18 -0500

    remove old results folders

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from datetime import date, timedelta
+import shutil
+
 import download_photos
 import upload_to_google_drive
 
 if __name__ == "__main__":
+    #delete yesterday's photos before downloading today's
+    yesterday = date.today() - timedelta(1)
+    shutil.rmtree("results/" + str(yesterday), ignore_errors=True)
+
     results_dir = download_photos.download_todays_media()
     print("Finished downloading photos. Now uploading to Google Drive...")
     upload_to_google_drive.upload_photos(results_dir)
