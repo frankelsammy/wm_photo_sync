511765ea79ba27a3abdfbb21e7707f8343914aac
Sammy Frankel <frankelsammy31@gmail.com>
2025-12-30 13:12:03 -0500

    auto

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import download_photos
 import upload_to_google_drive
 
 if __name__ == "__main__":
-    #delete yesterday's photos before downloading today's
-    yesterday = date.today() - timedelta(1)
+    #delete photos from two days ago before downloading today's
+    yesterday = date.today() - timedelta(2)
     shutil.rmtree("results/" + str(yesterday), ignore_errors=True)
+
+    #Check if any locations were schedulued for service yesterday and we didn't download any photos for them
 
     results_dir = download_photos.download_todays_media()
     print("Finished downloading photos. Now uploading to Google Drive...")
